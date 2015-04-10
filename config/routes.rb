7eee2aa3223d2eb561f1ccb3bae02c7bfd43a547
jancifejs <jan.smihla@gmail.com>
2015-04-10 22:26:51 +0200

    Add Controller admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  get 'admin/servers'
+
+  resources :tests
+
   get 'static_pages/home'
 
   get 'static_pages/help'
-
-  resources :posts
-
-  resources :users
-  root 'users#index'
+  #root 'users#index'
   #root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403164234) do
+ActiveRecord::Schema.define(version: 20150403172134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
